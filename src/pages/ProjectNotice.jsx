a3de0510c363d0616500ab06252dbc0c9b15267c
SumitSahu444 <sumitkumarsahu141@gmail.com>
2026-03-13 15:16:17 +0530

    done new change

--- src/pages/ProjectNotice.jsx
+++ src/pages/ProjectNotice.jsx
@@ -14,6 +14,7 @@ export default function ProjectNotice() {
         "Dynamic Website Developments",
         "Digital Marketing Services",
         "Software Development with Integrations",
+         "On-line Advertisement Services",
         "Mobile App Developments",
         "Outdoor Publicity & Advertisement Services",
         "Dynamic Website Tutorials",
